23d47e1e6d6251773ae0c0e0ce3b620abd4aa2e8
Dexiar <85329720+Dexiar@users.noreply.github.com>
2026-05-30 10:24:13 +0800

    Completed Task2.java

---
+++ activity/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte num1 = 0;
+        short num2 = 3;
+        int num3 = 11;
+        float f = 2.0f;
+        boolean b = true;
+
+        String output = "H" + num2 + num3 + num1 + " " + "w" + num1 + "rld" + " " + f + " " + b;
+
+        System.out.println(output);
+    }
+}
