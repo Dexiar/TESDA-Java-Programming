7e0c0ec283b94e3b2d1af75fc786b3e65472ea9e
Dexiar <85329720+Dexiar@users.noreply.github.com>
2026-06-05 15:32:13 +0800

    completed Task5.Java

--- activity/Task5.java
+++ activity/Task5.java
@@ -1,11 +1,14 @@
 //Finding the highest number
 
+
 import java.util.Scanner;
 
-public class Task3IfStatement {
+public class Task5 {
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-  
+       
+
         System.out.print("ENTER FIRST NUMBER: ");
         int num1 = scn.nextInt();
         System.out.print("ENTER SECOND NUMBER: ");
@@ -13,34 +16,17 @@ public class Task3IfStatement {
         System.out.print("ENTER THIRD NUMBER: ");
         int num3 = scn.nextInt();
 
-        if (num1==num2 && num1==num3) {
-            System.out.println("ALL NUMBERS ARE EQUAL");
-        }
-
-        else if (num1>num2 && num1>num3){
-            System.out.println("FIRST NUMBER IS LARGEST");
-        }
-
-        else if(num2>num1 && num2>num3){
-            System.out.println("SECOND NUMBER IS LARGEST");
-        }
-        
-        else{
-            System.out.println("THIRD NUMBER IS LARGEST");
-        }
-
+        System.out.println((num1==num2 && num1==num3)? "ALL NUMBERS ARE EQUAL":(num1>num2 && num1>num3)? "FIRST NUMBER IS LARGEST":(num2>num1 && num2>num3)? "SECOND NUMBER IS LARGEST":"THIRD NUMBER IS LARGEST");
     }
 }
 
+
 // import java.util.Scanner;
 
-
-// public class Task3 {
-
+// public class Task3IfStatement {
 //     public static void main(String[] args) {
 //         Scanner scn = new Scanner(System.in);
-       
-
+  
 //         System.out.print("ENTER FIRST NUMBER: ");
 //         int num1 = scn.nextInt();
 //         System.out.print("ENTER SECOND NUMBER: ");
@@ -48,6 +34,21 @@ public class Task3IfStatement {
 //         System.out.print("ENTER THIRD NUMBER: ");
 //         int num3 = scn.nextInt();
 
-//         System.out.println((num1==num2 && num1==num3)? "ALL NUMBERS ARE EQUAL":(num1>num2 && num1>num3)? "FIRST NUMBER IS LARGEST":(num2>num1 && num2>num3)? "SECOND NUMBER IS LARGEST":"THIRD NUMBER IS LARGEST");
+//         if (num1==num2 && num1==num3) {
+//             System.out.println("ALL NUMBERS ARE EQUAL");
+//         }
+
+//         else if (num1>num2 && num1>num3){
+//             System.out.println("FIRST NUMBER IS LARGEST");
+//         }
+
+//         else if(num2>num1 && num2>num3){
+//             System.out.println("SECOND NUMBER IS LARGEST");
+//         }
+        
+//         else{
+//             System.out.println("THIRD NUMBER IS LARGEST");
+//         }
+
 //     }
 // }
---
+++ chapter3/ArrayListLesson.java
@@ -0,0 +1,14 @@
+package chapter3;
+import java.util.*;
+
+public class ArrayListLesson {
+    public static void main(String[] args) {
+        ArrayList list1 = new ArrayList();
+        list1.add(1);
+        list1.add('c');
+        list1.add(true);
+        list1.add("LAN");
+        System.out.println(list1);
+
+    }
+}
