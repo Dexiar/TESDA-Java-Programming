4bc47ff21848297c326dfcefae702513f9dcc919
Dexiar <85329720+Dexiar@users.noreply.github.com>
2026-06-05 11:52:50 +0800

    Completed Task4.java

---
+++ activity/Task4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String input = scanner.nextLine();
+
+        StringBuilder sb = new StringBuilder(input);
+        String reversed = sb.reverse().toString();
+
+        System.out.println(
+            (input.equals(reversed))?"The input string is a palindrome":"The input string is not a palindrome"
+        );
+
+    }
+}
