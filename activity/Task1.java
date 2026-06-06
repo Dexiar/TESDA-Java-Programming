a39b2f7a2062d16ac1de267e89bc7247ed9f01fe
Dexiar <85329720+Dexiar@users.noreply.github.com>
2026-06-06 14:46:51 +0800

    Task1 edited

--- activity/Task1.java
+++ activity/Task1.java
@@ -1,5 +1,5 @@
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
