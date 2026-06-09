4df2f3efd60aa1f8195e0d8d1e909bf168d2da76
Dexiar <85329720+Dexiar@users.noreply.github.com>
2026-06-09 09:57:39 +0800

    Day22

--- otherFolder/hello.java
+++ otherFolder/hello.java
@@ -7,5 +7,20 @@ public class hello {
     public static void main(String[] args) {
         AccessModifier gm1 = new AccessModifier();
         gm1.greetMe();
+        // acm1.greetMe2();
+        // acm1.greetMe3();
+        // acm1.greetMe4();
+    }
+
+    
+}
+
+class MoonFlower extends AccessModifier{
+    public static void main(String[] args) {
+        MoonFlower mf = new MoonFlower();
+        mf.greetMe();
+        mf.greetMe3();
+        // mf.greetMe4();
+        
     }
 }
