68d1da2720408af113816c049d2efb6772c1fbf5
Dexiar <85329720+Dexiar@users.noreply.github.com>
2026-06-09 09:55:39 +0800

    Day 22

--- otherFolder/hello.java
+++ otherFolder/hello.java
@@ -4,5 +4,8 @@ import chapter4.AccessModifier;
 
 //rmp return type method name parenthesis 
 public class hello {
-    
+    public static void main(String[] args) {
+        AccessModifier gm1 = new AccessModifier();
+        gm1.greetMe();
+    }
 }
