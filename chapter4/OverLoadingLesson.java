8a2ed3fd547242490e89161e9ceacc417ab109b2
Dexiar <85329720+Dexiar@users.noreply.github.com>
2026-06-11 14:30:50 +0800

    Completed Overloading Lesson

--- chapter4/OverLoadingLesson.java
+++ chapter4/OverLoadingLesson.java
@@ -8,33 +8,46 @@ public class OverLoadingLesson {
     public void fly(short numFeet){
         System.out.println("This is a short");
     }
+    public void fly(Object obj){
+        System.out.println("This is a object");
+    }
 
     public boolean fly(){
         return false;
     }
 
-    void fly(int numMiles, short numFeet)throws Exception{
+    void fly( int numMiles, short numFeet){
         System.out.println("Int and Short");
     }
 
-    public void fly(short numFeet , int numMiles) {
-
-    }
+    public void fly(short numFeet , int numMiles) throws Exception {}
 
     public static void main(String[] args) {
         OverLoadingLesson oll = new OverLoadingLesson();
         short s = 1;
         oll.fly(1);
         oll.fly(s);
-        oll.fly();
         System.out.println(oll.fly());
         oll.fly(1,(byte)1);
-        try{
-            oll.fly((short)1, 1);
-        }
-
-        catch{
-            
-        }
+        oll.fly(new AccessModifier());
     }
 }
+
+class Glider2 {
+    public static String glide(String s) {
+      return "1";
+    }
+    public static String glide(String... s) {
+      return "2";
+    }
+    public static String glide(Object o) {
+      return "3";
+    }
+    public static String glide(String s, String t) {
+      return "4";
+    }
+    public static void main(String[] args) {
+      System.out.println(glide("a"));
+      System.out.println(glide("a", "b"));
+      System.out.println(glide("a", "b", "c"));
+    } }
